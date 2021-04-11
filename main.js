74738df3fc2b389b41dca08d429f5e33a725043b
Tom Phillips <eflips@yahoo.com>
2021-04-10 19:36:36 -0700

    Added onSale property to the object with v-show

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 7
+            inventory: 7,
+            onSale: true
         }
     }
 })
